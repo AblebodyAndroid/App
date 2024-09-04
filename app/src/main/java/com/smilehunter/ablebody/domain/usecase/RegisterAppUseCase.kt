c3c1c1805ff40297e6d965c967fc264e424086b4
agvber <agvber12@gmail.com>
2024-09-04 12:11:07 +0900

    refactor: OnboardingRepository data Layer 의존성 제거

---
+++ app/src/main/java/com/smilehunter/ablebody/domain/usecase/RegisterAppUseCase.kt
@@ -0,0 +1,26 @@
+package com.smilehunter.ablebody.domain.usecase
+
+import com.smilehunter.ablebody.data.model.Gender
+import com.smilehunter.ablebody.domain.repository.OnboardingRepository
+import javax.inject.Inject
+
+class RegisterAppUseCase @Inject constructor(
+    private val onboardingRepository: OnboardingRepository
+) {
+
+    suspend operator fun invoke(
+        gender: Gender,
+        nickname: String,
+        profileImage: Int,
+        verifyingCode: String,
+        agreeRequiredConsent: Boolean,
+        agreeMarketingConsent: Boolean
+    ): Boolean = onboardingRepository.createNewUser(
+        gender = gender,
+        nickname = nickname,
+        profileImage = profileImage,
+        verifyingCode = verifyingCode,
+        agreeRequiredConsent = agreeRequiredConsent,
+        agreeMarketingConsent = agreeMarketingConsent
+    )
+}
