98bb128f0a1bf6a6632116aa025e6f199f2e82d1
agvber <agvber12@gmail.com>
2024-09-05 16:57:05 +0900

    refactor: CommentRepository Data Layer 의존성 제거

--- app/src/main/java/com/smilehunter/ablebody/domain/repository/CommentRepository.kt
+++ app/src/main/java/com/smilehunter/ablebody/domain/repository/CommentRepository.kt
@@ -1,50 +1,26 @@
 package com.smilehunter.ablebody.domain.repository
 
-import com.smilehunter.ablebody.network.model.response.CreatorDetailCommentResponseData
-import com.smilehunter.ablebody.network.model.response.CreatorDetailLikeUsersResponseData
-import com.smilehunter.ablebody.network.model.response.CreatorDetailReplyResponseData
+import com.smilehunter.ablebody.domain.model.LikeListData
 
 interface CommentRepository {
 
-    suspend fun creatorDetailLikeBoard(
-        id: Long
-    )
+    suspend fun creatorDetailLikeBoard(id: Long)
 
-    suspend fun creatorDetailLikeComment(
-        id: Long
-    )
+    suspend fun creatorDetailLikeComment(id: Long)
 
-    suspend fun creatorDetailLikeReply(
-        id: Long
-    )
+    suspend fun creatorDetailLikeReply(id: Long)
 
-    suspend fun creatorDetailLikeUsersBoard(
-        id: Long
-    ): List<CreatorDetailLikeUsersResponseData>
+    suspend fun creatorDetailLikeUsersBoard(id: Long): List<LikeListData>
 
-    suspend fun creatorDetailLikeUsersComment(
-        id: Long
-    ): List<CreatorDetailLikeUsersResponseData>
+    suspend fun creatorDetailLikeUsersComment(id: Long): List<LikeListData>
 
-    suspend fun creatorDetailLikeUsersReply(
-        id: Long
-    ): List<CreatorDetailLikeUsersResponseData>
+    suspend fun creatorDetailLikeUsersReply(id: Long): List<LikeListData>
 
-    suspend fun creatorDetailComment(
-        id: Long,
-        content: String
-    ): CreatorDetailCommentResponseData
+    suspend fun creatorDetailComment(id: Long, content: String)
 
-    suspend fun creatorDetailReply(
-        id: Long,
-        content: String
-    ): CreatorDetailReplyResponseData
+    suspend fun creatorDetailReply(id: Long, content: String)
 
-    suspend fun creatorDetailDeleteComment(
-        id: Long
-    )
+    suspend fun creatorDetailDeleteComment(id: Long)
 
-    suspend fun creatorDetailDeleteReply(
-        id: Long
-    )
+    suspend fun creatorDetailDeleteReply(id: Long)
 }
