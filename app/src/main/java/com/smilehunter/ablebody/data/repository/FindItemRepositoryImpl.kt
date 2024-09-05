29ec48b6e638decd07d2f5dd8e5770a00fd4dd35
agvber <agvber12@gmail.com>
2024-09-05 16:14:34 +0900

    refactor: FindItemRepository data Layer 의존성 제거

---
+++ app/src/main/java/com/smilehunter/ablebody/data/repository/FindItemRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.smilehunter.ablebody.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.smilehunter.ablebody.data.mapper.toDomain
+import com.smilehunter.ablebody.data.model.ItemChildCategory
+import com.smilehunter.ablebody.data.model.ItemGender
+import com.smilehunter.ablebody.data.model.ItemParentCategory
+import com.smilehunter.ablebody.data.model.SortingMethod
+import com.smilehunter.ablebody.data.paging.CommonProductItemPagingSource
+import com.smilehunter.ablebody.domain.model.ProductItemData
+import com.smilehunter.ablebody.domain.repository.FindItemRepository
+import com.smilehunter.ablebody.network.NetworkService
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FindItemRepositoryImpl @Inject constructor(
+    private val networkService: NetworkService
+) : FindItemRepository {
+
+    override fun findItem(
+        sortingMethod: SortingMethod,
+        itemGender: ItemGender,
+        parentCategory: ItemParentCategory,
+        childCategory: ItemChildCategory?
+    ): Flow<PagingData<ProductItemData.Item>> {
+
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            initialKey = 0
+        ) {
+            CommonProductItemPagingSource { pageIndex ->
+                networkService.findItem(
+                    sort = sortingMethod,
+                    itemGender = itemGender,
+                    parentCategory = childCategory?.parentCategory ?: parentCategory,
+                    childCategory = childCategory,
+                    page = pageIndex,
+                ).body()!!.data!!.toDomain()
+            }
+        }
+            .flow
+    }
+
+}
