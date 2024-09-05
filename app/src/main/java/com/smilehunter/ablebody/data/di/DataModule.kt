e293aba3b5a84e6c5a83aca0c02adfbfdf5bcf5a
agvber <agvber12@gmail.com>
2024-09-05 15:55:59 +0900

    remove: FindItemRepository 파일 제거

--- app/src/main/java/com/smilehunter/ablebody/data/di/DataModule.kt
+++ app/src/main/java/com/smilehunter/ablebody/data/di/DataModule.kt
@@ -1,37 +1,35 @@
 package com.smilehunter.ablebody.data.di
 
-import com.smilehunter.ablebody.domain.repository.BookmarkRepository
 import com.smilehunter.ablebody.data.repository.BookmarkRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.BrandRepository
 import com.smilehunter.ablebody.data.repository.BrandRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.CommentRepository
 import com.smilehunter.ablebody.data.repository.CommentRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.CreatorDetailRepository
 import com.smilehunter.ablebody.data.repository.CreatorDetailRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.FCMSyncRepository
 import com.smilehunter.ablebody.data.repository.FCMSyncRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.FindCodyRepository
 import com.smilehunter.ablebody.data.repository.FindCodyRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.FindItemRepository
-import com.smilehunter.ablebody.data.repository.FindItemRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.ItemRepository
 import com.smilehunter.ablebody.data.repository.ItemRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.LikeListRepository
 import com.smilehunter.ablebody.data.repository.LikeListRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.ManageRepository
 import com.smilehunter.ablebody.data.repository.ManageRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.NotificationRepository
 import com.smilehunter.ablebody.data.repository.NotificationRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.OnboardingRepository
 import com.smilehunter.ablebody.data.repository.OnboardingRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.OrderManagementRepository
 import com.smilehunter.ablebody.data.repository.OrderManagementRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.SearchRepository
 import com.smilehunter.ablebody.data.repository.SearchRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.TokenRepository
 import com.smilehunter.ablebody.data.repository.TokenRepositoryImpl
-import com.smilehunter.ablebody.domain.repository.UserRepository
 import com.smilehunter.ablebody.data.repository.UserRepositoryImpl
+import com.smilehunter.ablebody.domain.repository.BookmarkRepository
+import com.smilehunter.ablebody.domain.repository.BrandRepository
+import com.smilehunter.ablebody.domain.repository.CommentRepository
+import com.smilehunter.ablebody.domain.repository.CreatorDetailRepository
+import com.smilehunter.ablebody.domain.repository.FCMSyncRepository
+import com.smilehunter.ablebody.domain.repository.FindCodyRepository
+import com.smilehunter.ablebody.domain.repository.ItemRepository
+import com.smilehunter.ablebody.domain.repository.LikeListRepository
+import com.smilehunter.ablebody.domain.repository.ManageRepository
+import com.smilehunter.ablebody.domain.repository.NotificationRepository
+import com.smilehunter.ablebody.domain.repository.OnboardingRepository
+import com.smilehunter.ablebody.domain.repository.OrderManagementRepository
+import com.smilehunter.ablebody.domain.repository.SearchRepository
+import com.smilehunter.ablebody.domain.repository.TokenRepository
+import com.smilehunter.ablebody.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -65,11 +63,6 @@ interface DataModule {
     fun bindsBookmarkRepository(
         bookmarkRepositoryImpl: BookmarkRepositoryImpl
     ): BookmarkRepository
-
-    @Binds
-    fun bindsFindItemRepository(
-        findItemRepositoryImpl: FindItemRepositoryImpl
-    ): FindItemRepository
 
     @Binds
     fun bindsFindCodyRepository(
